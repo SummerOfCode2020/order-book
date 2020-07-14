88040f04452c53ee992513661f382bfc2457dd35
erick-pacheco <linarespachecoerick@gmail.com>
2020-07-14 01:55:28 -0400

    Dependencies are installed and test file has been moved to a test folder

---
+++ orderBook.js
@@ -0,0 +1,16 @@
+ function reconcileOrder(existingBook, incomingOrder) {
+    existingBook = [{}];
+
+    if (incomingOrder.type.value() === 'sell' ) {
+        existingBook i
+    }
+
+};
+
+let order = reconcileOrder(1,2)
+console.assert(order === 'hellow', 'error')
+
+
+// using console.assert for testing.
+//module.exports = reconcileOrder
+
