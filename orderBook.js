442cc64614f7b8d10d475d4c097ff31a33da2400
Johnny Rice <jrice@labact.com>
2020-08-03 18:34:08 -0400

    Provide options for next step in this effort

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,13 @@
-function reconcileOrder () {
-    // adds an order to the book when the book is empty and thus cannot fulfill the order
+// The tests expects `reconcileOrder` function, we we have it named to match that expectation as set
+// The tests pass in two values, existingBook and incomingOrder
+//  - We can name these any names we would like here in the function declaration 
+//  - We can use the same naming here for these parameter names that we see in the test 
+function reconcileOrder (existingBook, incomingOrder) {
+    // to support @test "adds an order to the book when the book is empty and thus cannot fulfill the order"
+    if (existingBook.length === 0) {
+        // if the book is empty we can exit quickly here without needed to run further logic
+        return existingBook.concat({...incomingOrder})
+    }
 
 }
 
